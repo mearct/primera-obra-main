f37c52f87484361d063ee4a475281705d058e007
ian <mearct@gmail.com>
2022-09-20 09:47:11 +0800

    second commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Document Settings -->
+
+        <meta charset="utf-8"/>
+        <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="" />
+        <meta name="description" content="Primera-Obra" />
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="assets/images/ico/primera-obra_logo.jpg" sizes="16x16" />
+
+        <!-- Page Title -->
+        <title>PRIMERA-OBRA</title>
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+        <!-- Plugins -->
+        <link rel='stylesheet' href='assets/css/font-awesome.min.css'>
+        <link rel='stylesheet' href='assets/css/lightgallery.min.css'>
+        <link rel='stylesheet' href='assets/css/slick.css'>
+        <link rel='stylesheet' href='assets/css/zoom.css'>
+
+        <!-- Custom css file -->
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+
+    <!-- Wrapper -->
+        <div class="wrapper">
+        <!-- Header -->
+            <nav class="header row auto-hide-header page-transition">
+                <div class="header-inner">
+                    <div class="logo col-xs-10 col-md-2">
+                        <h3 href="index.html" class='logo-style'>
+                            PRIMERA-OBRA
+                        </h3>
+                    </div>
+                        <div class="col-xs-2 col-md-10 visible-xs"><a href ="#" class="nav-trigger"><span><em aria-hidden="true"></em>Menu</span></a></div>
+                    <!-- Menu -->
+                        <div class="menu col-sm-8 col-md-10" id="menu">
+                            <ul>
+                                <li><a href="index.html">home</a>
+                                 
+                                </li>
+                                <li><a href="about-me.html">about us</a></li>
+                                <li><a href="works.html">Works</a>
+                                 
+                                </li>
+                                <li><a href="gallery-images.html">Gallery</a></li>
+                                <li><a href="news.html">News</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                           </ul>
+                        </div>
+                    <!-- Menu -->
+                </div>
+            </nav>
+        <!-- /Header -->
+
+        <!-- Container -->
+            <main class="container">
+                <div class="home-slider">
+                <!-- Slider -->
+                    <div class="controls">
+                    <!-- Controls -->
+                        <div class="controls-navigate">
+                            <div class="prev">
+                                <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.5 980" style="enable-background:new 0 0 456.5 980;" xml:space="preserve">
+                                    <style type="text/css">.st0 {fill: #0FBFE6;}</style>
+                                    <path class="st0" d="M417,980c21.8,0,39.4-17.6,39.5-39.3c0-10.5-4.1-20.6-11.6-28L118.6,586.5c-53.2-53.2-53.2-139.7,0-192.8
+                                    L444.9,67.4c15.2-15.6,14.9-40.6-0.7-55.8c-15.3-14.9-39.7-14.9-55.1,0L62.9,337.8c-83.9,83.9-83.9,220.4,0,304.3l326.3,326.4
+                                    C396.6,975.9,406.6,980.1,417,980z" />
+                                </svg>
+                            </div>
+                            <div class="next">
+                                <svg version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.5 980" style="enable-background:new 0 0 456.5 980;" xml:space="preserve">
+                                    <style type="text/css">.st0 {fill: #0FBFE6;}</style>
+                                    <path class="st0" d="M39.5,0.1C17.7,0,0,17.6,0,39.4c0,10.5,4.1,20.6,11.6,28l326.3,326.3c53.2,53.2,53.2,139.7,0,192.8L11.6,912.7
+                                    c-15.6,15.2-15.9,40.2-0.7,55.8s40.2,15.9,55.8,0.7c0.2-0.2,0.5-0.5,0.7-0.7l326.2-326.3c83.9-83.9,83.9-220.4,0-304.3L67.2,11.5
+                                    C59.9,4.1,49.9,0,39.5,0.1z" />
+                                </svg>
+                            </div>
+                        </div>
+                    <!-- /Controls -->
+                    </div>
+                    <ul class="home-slider-horizontal">
+                        <li class="slide">
+                            <a href="index.html">
+                                <div class="s-bg" style="background-image: url(assets/images/gallery/image_01.png)"></div>
+                                <div class="slide-desc">
+                                    <h2>slide title</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="slide">
+                            <a href="index.html">
+                                <div class="s-bg" style="background-image: url(assets/images/gallery/image_02.png)"></div>
+                                <div class="slide-desc">
+                                    <h2>slide title</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="slide">
+                            <a href="index.html">
+                                <div class="s-bg" style="background-image: url(assets/images/gallery/image_03.png)"></div>
+                                <div class="slide-desc">
+                                    <h2>slide title</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="slide">
+                            <a href="index.html">
+                                <div class="s-bg" style="background-image: url(assets/images/gallery/image_04.png)"></div>
+                                <div class="slide-desc">
+                                    <h2>slide title</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="slide">
+                            <a href="index.html">
+                                <div class="s-bg" style="background-image: url(assets/images/gallery/image_05.png)"></div>
+                                <div class="slide-desc">
+                                    <h2>slide title</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="slide">
+                            <a href="index.html">
+                                <div class="s-bg" style="background-image: url(assets/images/gallery/image_06.png)"></div>
+                                <div class="slide-desc">
+                                    <h2>slide title</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="slide">
+                            <a href="index.html">
+                                <div class="s-bg" style="background-image: url(assets/images/gallery/image_07.png)"></div>
+                                <div class="slide-desc">
+                                    <h2>slide title</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="slide">
+                            <a href="index.html">
+                                <div class="s-bg" style="background-image: url(assets/images/gallery/image_08.png)"></div>
+                                <div class="slide-desc">
+                                    <h2>slide title</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="slide">
+                            <a href="index.html">
+                                <div class="s-bg" style="background-image: url(assets/images/gallery/image_09.png)"></div>
+                                <div class="slide-desc">
+                                    <h2>slide title</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="slide">
+                            <a href="index.html">
+                                <div class="s-bg" style="background-image: url(assets/images/gallery/image_10.png)"></div>
+                                <div class="slide-desc">
+                                    <h2>slide title</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="slide">
+                            <a href="index.html">
+                                <div class="s-bg" style="background-image: url(assets/images/gallery/image_11.png)"></div>
+                                <div class="slide-desc">
+                                    <h2>slide title</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="slide">
+                            <a href="index.html">
+                                <div class="s-bg" style="background-image: url(assets/images/gallery/image_12.png)"></div>
+                                <div class="slide-desc">
+                                    <h2>slide title</h2>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                <!-- /Slider -->
+                </div>
+            </main>
+        <!-- /Container -->
+            
+         <!-- Footer -->
+         <footer id="footer">
+            <div class="row">
+                <p class="col-xs-12 col-sm-6 col-md-6">Copyright © 2022 All Rights Reserved.</p>
+                <ul class="follow col-xs-6 col-sm-6 col-md-6 hidden-xs">
+                    <li><p>Follow Us</p></li>
+                    <li><span></span></li>
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                    <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                    <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                </ul>
+                </div>
+            </footer>
+        <!-- /Footer -->
+        </div>
+    <!-- /Wrapper -->
+
+    <!-- JS -->
+    <!-- jquery-2.1.3.min js -->
+        <script type="text/javascript" src='assets/js/jquery-2.1.3.min.js'></script>
+
+    <!-- Plugins -->
+        <script type="text/javascript" src='assets/js/libs.js'></script>
+
+    <!-- Main js -->
+        <script type="text/javascript" src="assets/js/main.js"></script>
+    </body>
+</html>
